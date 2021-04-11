172afe0b3e1284ba15e82308b2eee326b850f0e6
Greta <greta.carrete@vizzuality.com>
2021-04-11 22:11:08 +0200

    scrolling option added with css

--- app.R
+++ app.R
@@ -5,10 +5,16 @@ library(ggplot2)
 source("get_data.R")
 
 ui = fluidPage(
+  tags$head(
+    tags$style("html, body { height: 100%; width: 100%}"),
+    #tags$style("#panel2 {overflow: auto; }"),
+    tags$style("#panel1 {height: 100%;background: #ADD8E6}")
+    
+  ),
   #inputs
   titlePanel("Vaccine Confidence Project Shiny App"),
   sidebarLayout(
-    sidebarPanel(width = 2,
+    sidebarPanel(width = 2,id = "panel1",
                  p("Data source: ",
                    a("Figueiredo et al. 2020", 
                      href = "https://www.thelancet.com/journals/lancet/article/PIIS0140-6736(20)31558-0/fulltext"), "Mapping global trends in vaccine confidence and investigating barriers to vaccine uptake: a large-scale retrospective temporal modelling study. ", em("The Lancet")),
@@ -16,7 +22,7 @@ ui = fluidPage(
                  selectInput(inputId="regions", label="Region", choices = all_regions, selected="EUR")
     ),
     #outputs
-    mainPanel(width = 10,
+    mainPanel(width = 10,id = "panel2",
               
               tabsetPanel(type = "tabs",
                           tabPanel("Safe", plotOutput(outputId="year_range_plot_safe")),
@@ -39,13 +45,22 @@ server = function(input, output){
     ggplot(data = dd , 
            aes(x= time, y=mean, group=response)) + 
       geom_line(aes( colour= response))+
-      geom_ribbon(aes(ymin=X95.HPDlow, ymax=X95.HPDhigh), linetype=2, alpha=0.1)+
-      facet_wrap(~country.or.territory, ncol=10)+
+      #geom_point(aes( colour= response), shape = 21)+
+      #geom_label(data = dd %>% filter(time %in% c(min(dd$time),max(dd$time))), 
+      #          aes( fill= response, label = round(mean)), size = 3, show.legend = FALSE)+
+      geom_ribbon(aes(ymin=X95.HPDlow, ymax=X95.HPDhigh, fill = response), alpha=0.1)+
+      facet_wrap(~country.or.territory)+
       ggtitle("Are vaccines safe?")+
-      theme_bw()+
+      coord_cartesian(clip = 'off', expand = TRUE)+
+      theme_classic()+
       theme(legend.position = "top")
+      #theme(legend.position = "top", 
+            #axis.title.y=element_blank(),
+            #axis.text.y=element_blank(),
+            #axis.ticks.y=element_blank(), 
+            #panel.spacing = unit(1, "lines"))
   }, width = "auto",
-  height = 600)
+  height = 650)
   
   output$year_range_plot_imp<- renderPlot({
     
@@ -55,14 +70,14 @@ server = function(input, output){
     
     ggplot(data = dd , 
            aes(x= time, y=mean, group=response)) + 
-      geom_line(aes( colour= response))+
-      geom_ribbon(aes(ymin=X95.HPDlow, ymax=X95.HPDhigh), linetype=2, alpha=0.1)+
-      facet_wrap(~country.or.territory, ncol=10)+
+      geom_line(aes(colour= response))+
+      geom_ribbon(aes(ymin=X95.HPDlow, ymax=X95.HPDhigh, fill = response), alpha=0.1)+
+      facet_wrap(~country.or.territory)+
       ggtitle("Are vaccines important?")+
-      theme_bw()+
+      theme_classic()+
       theme(legend.position = "top")
   }, width = "auto",
-  height = 600)
+  height = 650)
   
   output$year_range_plot_eff <- renderPlot({
     
@@ -73,13 +88,13 @@ server = function(input, output){
     ggplot(data = dd , 
            aes(x= time, y=mean, group=response)) + 
       geom_line(aes( colour= response))+
-      geom_ribbon(aes(ymin=X95.HPDlow, ymax=X95.HPDhigh), linetype=2, alpha=0.1)+
-      facet_wrap(~country.or.territory, ncol=10)+
+      geom_ribbon(aes(ymin=X95.HPDlow, ymax=X95.HPDhigh, fill = response), alpha=0.1)+
+      facet_wrap(~country.or.territory)+
       ggtitle("Are vaccines efficient?")+
-      theme_bw()+
+      theme_classic()+
       theme(legend.position = "top")
   }, width = "auto",
-  height = 600)
+  height = 650)
   
 }
 
